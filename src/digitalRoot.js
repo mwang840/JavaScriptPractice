f750a0f81271631de85120c9fad9a02b5cd471bc
Maxwell Wang <maxwang@udel.edu>
2022-05-09 12:02:51 -0400

    made prettier detect

--- src/digitalRoot.js
+++ src/digitalRoot.js
@@ -1,4 +1,11 @@
-export function digitalRoots(n){
-    
-    return n;
+export function digital_root(n){
+    //Chacks for single digit which is base case
+    if(n == 0){
+        return 0;
+    }
+    //Makes a recursive call
+    else{
+        return digital_root(n % 10);
+        n = n/10;
+    }
 }
