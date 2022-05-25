39cc3b3f5982a284d162531c0b4058c04cb0e12d
mwang840 <maxwang@udel.edu>
2022-05-25 10:50:00 -0400

    splitted and iterated over a loop

--- src/reverseWords.js
+++ src/reverseWords.js
@@ -1,9 +1,8 @@
-export function reverseWords(word){
-    let reversed = []
-    input = word.match(/S\+/g)
-    var answer = ""
-    for(let i = 0; i < word.length; ++i){
-        answer += reversed(word[i]).split(' ').reverse().join(' ') + " "
+function reverseWords(str){
+    var splitted = str.split(" ")
+    var reversed = []
+    for(let i = splitted.length; i >=0; i--){
+        reversed.push(splitted[i])
     }
-    return answer
+    return reversed.join("")
 }
