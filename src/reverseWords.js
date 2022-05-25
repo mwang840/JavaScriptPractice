2c773a4257d0d31e94a092a6cd33a063b74420e3
mwang840 <maxwang@udel.edu>
2022-05-25 10:06:22 -0400

    turned it to a one liner with split reverse and join methods

--- src/reverseWords.js
+++ src/reverseWords.js
@@ -1,10 +1,3 @@
 export function reverseWords(word){
-    const splitBySpace = word.split(" ");
-    let backWards = "";
-    for(let i = 0; i < word.length(); ++i){
-        if(word[i] != " "){
-            backWards.join(splitBySpace);
-        }
-    }
-    return backWards;
+    return word.split(" ").reverse().join("");
 }
