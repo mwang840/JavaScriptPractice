cc97504a00022f6eb8bfe6a5fb664e66ec71e974
Maxwell Wang <maxwang@udel.edu>
2022-06-18 21:16:34 -0400

    split strings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { solution  } from "./splitStrings";
 function index(){
     console.log("Hello World");
+    console.log(solution('abc'));
 }
---
+++ src/splitStrings.js
@@ -0,0 +1,16 @@
+export function solution(str){
+    holder = []
+    for(var i = 0; i < str.length(); ++i){
+        if(i % 2 == 0){
+            holder += str.split(/[,]+/)
+            holder+=str[i]
+        }
+        else{
+            holder+= str[i]
+        }
+    }
+    if(str.length() % 2 == 1){
+        holder.push('_')
+    }
+    return holder
+}
